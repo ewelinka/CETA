794cd7c7f651b17d355254d3373c346cd1d315e5
ewe <ewelina.bakala@gmail.com>
2016-07-19 08:50:03 -0300

    inital files for libgdx game generated with lingdx project generator

---
+++ android/src/ceta/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ceta.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ceta.game.CetaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CetaGame(), config);
+	}
+}
---
+++ desktop/src/ceta/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ceta.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ceta.game.CetaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CetaGame(), config);
+	}
+}
