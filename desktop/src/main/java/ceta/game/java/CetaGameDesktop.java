4d2e74b0982eb4e3e71ab92131f0a950543bc450
ewe <ewelina.bakala@gmail.com>
2016-09-29 22:09:26 -0300

    merge and clean

--- desktop/src/main/java/ceta/game/java/CetaGameDesktop.java
+++ desktop/src/main/java/ceta/game/java/CetaGameDesktop.java
@@ -3,11 +3,13 @@ package ceta.game.java;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import ceta.game.core.CetaGame;
+import ceta.game.CetaGame;
 
 public class CetaGameDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 768;
+		config.width = 450;
 		new LwjglApplication(new CetaGame(), config);
 	}
 }
