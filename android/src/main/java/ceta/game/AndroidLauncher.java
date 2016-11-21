b8a84d2f9207a35b89a09b9d9a0274bc475b3208
ewe <ewelina.bakala@gmail.com>
2016-11-21 14:31:19 -0300

    fast level change

--- android/src/main/java/ceta/game/AndroidLauncher.java
+++ android/src/main/java/ceta/game/AndroidLauncher.java
@@ -11,16 +11,21 @@ import ceta.game.util.osc.OSCReceiver;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.ceta.vision.android.topcode.TopCodeDetectorAndroid;
+
 
 public class AndroidLauncher extends AndroidApplication {
 	public static final String TAG = AndroidLauncher.class.getName();
+	public TopCodeDetectorAndroid topCodeDetector;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		//config.hideStatusBar=false;
+		topCodeDetector = new TopCodeDetectorAndroid(40,true,70,5,true,false);
 		initialize(new CetaGame(), config);
+
 	}
 	
 }
